ab53ab8edd26115256aedbcc56b61c19bb604cca
Johan <jhoan_estiven2@hotmail.es>
2024-11-14 21:03:24 -0500

    Muestra Store Procedure Separado

--- Documentos/SP/SP_Clientes.sql
+++ Documentos/SP/SP_Clientes.sql
@@ -1,11 +1,6 @@
 DELIMITER $$
-CREATE PROCEDURE SP_Clientes_SIUD(
+CREATE PROCEDURE SP_Clientes_Listar(
     IN p_ClienteID INT,
-    IN p_Nombre VARCHAR(100),
-    IN p_Apellido VARCHAR(100),
-    IN p_Correo VARCHAR(100),
-    IN p_Direccion VARCHAR(200),
-    IN p_Telefono VARCHAR(15),
     OUT p_resultado VARCHAR(255)
 )
 BEGIN
@@ -26,6 +21,30 @@ BEGIN
     ELSE
         SELECT * FROM Clientes;
     END IF;
+
+END $$
+DELIMITER ;
+
+DELIMITER $$
+CREATE PROCEDURE SP_Clientes_Insertar(
+    IN p_Nombre VARCHAR(100),
+    IN p_Apellido VARCHAR(100),
+    IN p_Correo VARCHAR(100),
+    IN p_Direccion VARCHAR(200),
+    IN p_Telefono VARCHAR(15),
+    OUT p_resultado VARCHAR(255)
+)
+BEGIN
+    DECLARE v_error INT DEFAULT 0;
+    DECLARE v_error_message VARCHAR(255);
+
+    -- MANEJAR EXCEPCIONES
+    DECLARE CONTINUE HANDLER FOR SQLEXCEPTION 
+    BEGIN
+        GET DIAGNOSTICS CONDITION 1 v_error_message = MESSAGE_TEXT;
+        SET p_resultado = CONCAT('Error: ', v_error_message);
+        SET v_error = 1;
+    END;
     
     -- INSERT
     INSERT INTO Clientes (Nombre, Apellido, Correo, Direccion, Telefono)
@@ -34,6 +53,31 @@ BEGIN
     IF v_error = 0 THEN
         SET p_resultado = 'Cliente ingresado correctamente';
     END IF;
+
+END $$
+DELIMITER ;
+
+DELIMITER $$
+CREATE PROCEDURE SP_Clientes_Actualizar(
+    IN p_ClienteID INT,
+    IN p_Nombre VARCHAR(100),
+    IN p_Apellido VARCHAR(100),
+    IN p_Correo VARCHAR(100),
+    IN p_Direccion VARCHAR(200),
+    IN p_Telefono VARCHAR(15),
+    OUT p_resultado VARCHAR(255)
+)
+BEGIN
+    DECLARE v_error INT DEFAULT 0;
+    DECLARE v_error_message VARCHAR(255);
+
+    -- MANEJAR EXCEPCIONES
+    DECLARE CONTINUE HANDLER FOR SQLEXCEPTION 
+    BEGIN
+        GET DIAGNOSTICS CONDITION 1 v_error_message = MESSAGE_TEXT;
+        SET p_resultado = CONCAT('Error: ', v_error_message);
+        SET v_error = 1;
+    END;
     
     -- UPDATE
     UPDATE Clientes
@@ -51,6 +95,26 @@ BEGIN
             SET p_resultado = 'Cliente actualizado correctamente';
         END IF;
     END IF;
+
+END $$
+DELIMITER ;
+
+DELIMITER $$
+CREATE PROCEDURE SP_Clientes_Eliminar(
+    IN p_ClienteID INT,
+    OUT p_resultado VARCHAR(255)
+)
+BEGIN
+    DECLARE v_error INT DEFAULT 0;
+    DECLARE v_error_message VARCHAR(255);
+
+    -- MANEJAR EXCEPCIONES
+    DECLARE CONTINUE HANDLER FOR SQLEXCEPTION 
+    BEGIN
+        GET DIAGNOSTICS CONDITION 1 v_error_message = MESSAGE_TEXT;
+        SET p_resultado = CONCAT('Error: ', v_error_message);
+        SET v_error = 1;
+    END;
 
     -- DELETE
     DELETE FROM Clientes WHERE ClienteID = p_ClienteID;
