d6008cdc3e3316fb8e34186282545fcdf8bdad29
DiegoArias <diegoarias245794@correo.itm.edu.co>
2024-11-17 00:16:16 -0500

    Agrego Eliminar,Actualizar,Listar,listarTodo.Insertar de Empleados

---
+++ Aplicaciones/cl_Empleado_Aplicacion.py
@@ -0,0 +1,56 @@
+from Nucleo.cl_Empleado import cl_Empleado;
+from Repositorios.cl_Empleado_Repositorio import cl_Empleado_Repositorio;
+
+class cl_Empleado_Aplicacion:
+
+    def __init__(self):
+        self.respositorio = cl_Empleado_Repositorio();
+
+    def Listar(self, id: str) -> None:
+        return self.respositorio.Listar(id);
+
+    def Insertar(self, datos: dict) -> None:
+        respuesta: dict = { };
+
+        if(not "Nombre" in datos.keys() or 
+           not "Apellido" in datos.keys() or 
+           not "Correo" in datos.keys() or 
+           not "Telefono" in datos.keys()):
+            
+            respuesta["Error"] = "Falta informacion";
+            return respuesta;
+        
+        empleado: cl_Empleado = cl_Empleado();
+        empleado.SetNombre(datos["Nombre"]);
+        empleado.SetApellido(datos["Apellido"]);
+        empleado.SetCorreo(datos["Correo"]);
+        empleado.SetTelefono(datos["Telefono"]);
+        
+
+        return self.respositorio.Insertar(empleado);
+
+
+    def Actualizar(self, datos: dict) -> None:
+            respuesta: dict = { };
+
+            if(not "EmpleadoID" in datos.keys() or
+            not "Nombre" in datos.keys() or 
+            not "Apellido" in datos.keys() or 
+            not "Correo" in datos.keys() or 
+            not "Telefono" in datos.keys()): 
+                
+                respuesta["Error"] = "Falta informacion";
+                return respuesta;
+            
+            empleado: cl_Empleado = cl_Empleado();
+            empleado.SetEmpleadoID(datos["EmpleadoID"]);
+            empleado.SetNombre(datos["Nombre"]);
+            empleado.SetApellido(datos["Apellido"]);
+            empleado.SetCorreo(datos["Correo"]);
+            empleado.SetTelefono(datos["Telefono"]);
+            
+            return self.respositorio.Actualizar(empleado);
+
+
+    def Eliminar(self, id: str) -> None:
+            return self.respositorio.Eliminar(id);
