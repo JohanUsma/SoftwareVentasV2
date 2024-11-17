6a02de6ef47137afa48851399fb349c56adcdac8
JFelipe <juanchaves209202@correo.itm.edu.co>
2024-11-17 00:09:21 -0500

    Terminación Repo, Aplicación y Servicio de MetodoPago

---
+++ Aplicaciones/cl_MetodoPago_Aplicacion.py
@@ -0,0 +1,43 @@
+from Nucleo.cl_Cliente import cl_Cliente;
+from Repositorios.cl_Cliente_Repositorio import cl_Cliente_Repositorio;
+from Nucleo.cl_MetodoPago import cl_MetodoPago;
+from Repositorios.cl_MetodoPago_Repositorio import cl_MetodoPago_Repositorio;
+
+class cl_MetodoPago_Aplicacion:
+
+    def __init__(self):
+        self.respositorio = cl_MetodoPago_Repositorio();
+
+    def Listar(self, id: str) -> None:
+        return self.respositorio.Listar(id);
+    
+    def Insertar(self, datos: dict) -> None:
+        respuesta: dict = { };
+
+        if(not "Nombre" in datos.keys()): 
+            
+            respuesta["Error"] = "Falta informacion";
+            return respuesta;
+        
+        metodoPago: cl_MetodoPago = cl_MetodoPago();
+        metodoPago.SetNombre(datos["Nombre"]);
+
+        return self.respositorio.Insertar(metodoPago);
+    
+    def Actualizar(self, datos: dict) -> None:
+        respuesta: dict = { };
+
+        if(not "MetodoPagoID" in datos.keys() or
+           not "Nombre" in datos.keys()): 
+            
+            respuesta["Error"] = "Falta informacion";
+            return respuesta;
+        
+        metodoPago: cl_MetodoPago = cl_MetodoPago();
+        metodoPago.SetMetodoPagoID(datos["MetodoPagoID"]);
+        metodoPago.SetNombre(datos["Nombre"]);
+
+        return self.respositorio.Actualizar(metodoPago);
+
+    def Eliminar(self, id: str) -> None:
+        return self.respositorio.Eliminar(id);
---
+++ Repositorios/cl_MetodoPago_Repositorio.py
@@ -0,0 +1,104 @@
+import pyodbc;
+from Utilidades.cl_Database import cl_Database; 
+from Nucleo.cl_MetodoPago import cl_MetodoPago;
+
+class cl_MetodoPago_Repositorio:
+    
+    def Listar(self, id: str) -> dict:  
+        respuesta = { };
+        try:
+            
+            conexion = pyodbc.connect(cl_Database.strConnection);
+            
+            if(not id):
+                consulta: str = f"CALL SP_MetodosPago_Listar(NULL, @p_resultado);";
+            else: 
+                consulta: str = f"CALL SP_MetodosPago_Listar({id}, @p_resultado);";
+            
+            cursor = conexion.cursor();
+            cursor.execute(consulta);
+
+            respuesta: dict = { };
+            contador = 0;
+            for elemento in cursor:
+                #LLENAR LISTA DE MetodoPago
+                metodoPago: cl_MetodoPago = cl_MetodoPago();
+                metodoPago.SetMetodoPagoID(elemento[0]);
+                metodoPago.SetNombre(elemento[1]);
+                respuesta[str(contador)] = metodoPago.__dict__;
+                contador = contador + 1;
+                
+            cursor.close();
+            conexion.close();
+            return respuesta;
+        except Exception as ex:
+            respuesta["Error"] = str(ex);
+            return respuesta;
+    
+    def Insertar(self, metodoPago: cl_MetodoPago) -> dict:  
+        respuesta = { };
+        try:
+            
+            conexion = pyodbc.connect(cl_Database.strConnection);
+            cursor = conexion.cursor();
+            
+            consulta: str = "{CALL SP_MetodosPago_Insertar( ";
+            consulta += "'" + metodoPago.GetNombre() + "', @Resultado);}";
+            cursor.execute(consulta);
+            
+            consulta: str = "SELECT @Resultado;";
+            cursor.execute(consulta);
+            respuesta["Resultado"] = str(cursor.fetchone()[0]);
+            cursor.execute("commit;");
+
+            cursor.close();
+            conexion.close();
+            return respuesta;
+        except Exception as ex:
+            respuesta["Error"] = str(ex);
+            return respuesta;
+    
+    def Actualizar(self, metodoPago: cl_MetodoPago) -> dict:  
+        respuesta = { };
+        try:
+            
+            conexion = pyodbc.connect(cl_Database.strConnection);
+            cursor = conexion.cursor();
+            
+            consulta: str = "{CALL SP_MetodosPago_Actualizar( ";
+            consulta += "'" + metodoPago.GetMetodoPagoID() + "', '" + metodoPago.GetNombre() + "', @Resultado);}";
+            cursor.execute(consulta);
+            
+            consulta: str = "SELECT @Resultado;";
+            cursor.execute(consulta);
+            respuesta["Resultado"] = str(cursor.fetchone()[0]);
+            cursor.execute("commit;");
+
+            cursor.close();
+            conexion.close();
+            return respuesta;
+        except Exception as ex:
+            respuesta["Error"] = str(ex);
+            return respuesta;
+    
+    def Eliminar(self, id: str) -> dict:  
+        respuesta = { };
+        try:
+            
+            conexion = pyodbc.connect(cl_Database.strConnection);
+            cursor = conexion.cursor();
+            
+            consulta: str = f"CALL SP_MetodosPago_Eliminar({id}, @Resultado);";
+            cursor.execute(consulta);
+            
+            consulta: str = "SELECT @Resultado;";
+            cursor.execute(consulta);
+            respuesta["Resultado"] = str(cursor.fetchone()[0]);
+            cursor.execute("commit;");
+
+            cursor.close();
+            conexion.close();
+            return respuesta;
+        except Exception as ex:
+            respuesta["Error"] = str(ex);
+            return respuesta;
